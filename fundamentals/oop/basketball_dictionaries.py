40649fd1b1212c376040fea8527fbe2381dc7a26
blake-jensen99 <blazeba99@gmail.com>
2023-04-04 14:44:41 -0500

    New Assignment

---
+++ fundamentals/oop/basketball_dictionaries.py
@@ -0,0 +1,80 @@
+players = [
+    {
+        "name": "Kevin Durant",
+        "age": 34,
+        "position": "small forward",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum",
+        "age": 24,
+        "position": "small forward",
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving",
+        "age": 32,
+        "position": "Point Guard",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard",
+        "age": 33,
+        "position": "Point Guard",
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid",
+        "age": 32,
+        "position": "Power Foward",
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+
+class Player:
+    def __init__(self, name, age, position, team):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.team = team
+
+
+roster = []
+for i in players:
+    person = Player(i["name"], i["age"], i["position"], i["team"])
+    roster.append(person)
+
+for i in range(0, len(roster)):
+    print(roster[i].name)
+
+
+kevin = {
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets"
+}
+jason = {
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics"
+}
+kyrie = {
+    "name": "Kyrie Irving",
+    "age": 32,
+    "position": "Point Guard",
+    "team": "Brooklyn Nets"
+}
+
+# Create your Player instances here!
+player_kevin = Player(kevin["name"], kevin["age"], kevin["position"], kevin["team"])
+player_jason = Player(jason["name"], jason["age"], jason["position"], jason["team"])
+player_kyrie = Player(kyrie["name"], kyrie["age"], kyrie["position"], kyrie["team"])
